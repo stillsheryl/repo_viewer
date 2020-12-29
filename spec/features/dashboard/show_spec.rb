35ace62aa7400e4ba134aaf5a4bb6441393e5b88
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-29 15:49:41 -0700

    WIP - test for logging in

--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -2,33 +2,26 @@ require 'rails_helper'
 
 describe "As a visitor when I log in" do
   it "shows a welcome message" do
+    stub_omniauth
+require "pry"; binding.pry
     visit root_path
 
     click_on "Login with Github"
 
-
+    expect(page).to have_content("Welcome, Shezza!")
   end
+
+  def stub_omniauth
+      OmniAuth.config.test_mode = true
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+        uid: "12345678910",
+        info: {
+          username: "Shezza"
+        },
+        credentials: {
+          token: "abcdefg12345"
+        }
+      })
+    end
 end
-
-
-
-def stub_omniauth
-    # first, set OmniAuth to run in test mode
-    OmniAuth.config.test_mode = true
-    # then, provide a set of fake oauth data that
-    # omniauth will use when a user tries to authenticate:
-    OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
-      provider: "google",
-      uid: "12345678910",
-      info: {
-        email: "jesse@mountainmantechnologies.com",
-        first_name: "Jesse",
-        last_name: "Spevack"
-      },
-      credentials: {
-        token: "abcdefg12345",
-        refresh_token: "12345abcdefg",
-        expires_at: DateTime.now,
-      }
-    })
-  end
