bb2cfce934e810506654d1601dc8bea8515cd383
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-30 11:23:38 -0700

    Add link and route to view public repos

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get '/auth/github/callback', to: 'sessions#create'
   get '/dashboard', to: 'dashboard#show'
 
+  get '/users/:user/repos', to: 'repos#show'
+
 end
