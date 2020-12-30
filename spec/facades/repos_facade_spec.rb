0f5c3f3555b20b963b28b7355640b44350731d70
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-30 14:45:13 -0700

    Add test for private repos method

--- spec/facades/repos_facade_spec.rb
+++ spec/facades/repos_facade_spec.rb
@@ -16,4 +16,20 @@ describe "ReposFacade" do
       expect(first_repo.url).to be_a(String)
     end
   end
+
+  describe "private_repos" do
+    it "returns an array of repo ojects for a given user" do
+      user = "stillsheryl"
+
+      repos = ReposFacade.public_repos(user)
+
+      expect(repos).to be_an(Array)
+
+      first_repo = repos.first
+
+      expect(first_repo).to be_a(Repo)
+      expect(first_repo.name).to be_a(String)
+      expect(first_repo.url).to be_a(String)
+    end
+  end
 end
