acbafd105fc76b695145a348fbca9627de59aaaa
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-12-30 14:56:33 -0700

    Add repo service for private repos

--- app/services/repo_service.rb
+++ app/services/repo_service.rb
@@ -4,4 +4,16 @@ class RepoService
 
     repos = JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.private_repos(user, token)
+    response = Faraday.get("https://api.github.com/user/repos?per_page=1000") do |req|
+      req.headers['Authorization'] = "Bearer #{token}"
+    end
+
+    repos_data = JSON.parse(response.body, symbolize_names: true)
+
+    repos = repos_data.find_all do |repo|
+      repo[:private] == true
+    end
+  end
 end
